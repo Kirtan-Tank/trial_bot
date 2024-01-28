c56afc212f5ed717585b4a82ae863c3c1c02fbbc
Kirtan Tank <89449817+Kirtan-Tank@users.noreply.github.com>
2024-01-28 22:31:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def main():
         if user_question:
             docs = knowledge_base.similarity_search(user_question, top_k=3)
             st.write("docs end")
-            llm = HuggingFaceHub(repo_id="Qiliang/bart-large-cnn-samsum-ChatGPT_v3", model_kwargs={"temperature":3, "max_length":5000})
+            llm = HuggingFaceHub(repo_id="Qiliang/bart-large-cnn-samsum-ChatGPT_v3", model_kwargs={"temperature":1, "max_length":5000})
             st.write("llm end")
             chain = load_qa_chain(llm, chain_type="stuff")
             st.write("chain loaded")
