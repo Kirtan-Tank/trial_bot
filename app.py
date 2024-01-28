4eeb9e8e708c14bca98c785493ad0823687c2c80
Kirtan Tank <89449817+Kirtan-Tank@users.noreply.github.com>
2024-01-28 22:16:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def main():
         if user_question:
             docs = knowledge_base.similarity_search(user_question, top_k=3)
             st.write("docs end")
-            llm = HuggingFaceHub(repo_id="Rostlab/prot_t5_xl_uniref50", model_kwargs={"temperature":3, "max_length":2000})
+            llm = HuggingFaceHub(repo_id="facebook/m2m100_418M", model_kwargs={"temperature":3, "max_length":2000})
             st.write("llm end")
             chain = load_qa_chain(llm, chain_type="stuff")
             st.write("chain loaded")
